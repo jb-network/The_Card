98b80315a1902699067b9b23503a2365dc182ccd
jb-network <85036487+jb-network@users.noreply.github.com>
2022-06-09 23:43:30 -0500

    Add project files.

---
+++ The_Card/Program.cs
@@ -0,0 +1,24 @@
+﻿// This is challenge work for the "C# Players Guide"
+// Level 24 "The Card" Challenge
+// Create a cards class that accomidates the following:
+// - Each card has one of the following colors Red, Green, Blue, Yellow
+// - Include a rank for each card (1 - 10, followed by $, %, and &)
+
+
+
+
+
+public class Card
+{
+    public CardColor Color { get; }
+    public CardRank Rank { get; }
+
+    public Card(CardColor color, CardRank rank)
+    {
+        Color = color;
+        Rank = rank;
+    }
+
+    public enum CardColor { Red, Green, Blue, Yellow }
+    public enum CardRank { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Dollar, Precent, Caret, Ampersand }
+}
